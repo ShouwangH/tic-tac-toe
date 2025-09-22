69a75b9d2bd4032e67054f5f624b02177511a1f7
Shouwang Huang <shouwang@mac.mynetworksettings.com>
2025-09-22 13:43:38 -0400

    initial display install

---
+++ reactapp/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1>Tic Tac Toe</h1>
+      <div className="container">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      
+        
+
+      
+    </>
+  )
+}
+
+export default App
